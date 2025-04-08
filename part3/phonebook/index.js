5c863c9c5f3f74a5d8ebc842586b6b0afedf3d63
nickleezx <nickleezx@gmail.com>
2025-04-08 20:16:12 +0800

    ex 3.7-3.8 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+
 app.use(express.json());
+
+const logger = morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens['posted-msg'] = JSON.stringify(req.body),
+    ].join(' ')
+  })
+
+app.use(logger);
+
 
 let phoneBook = [
     { 
@@ -25,6 +41,7 @@ let phoneBook = [
       "number": "39-23-6423122"
     }
 ]
+
 
 app.get('/api/persons', (request, response) => {
     response.json(phoneBook);
@@ -84,6 +101,10 @@ app.post('/api/persons', (request, response) => {
     phoneBook = phoneBook.concat(person)
     response.json(person);
 })
+
+
+
+
 
 const PORT = 3001;
 app.listen(PORT, () => {

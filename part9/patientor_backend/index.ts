2a58813a846f00fbbeb4e24cd8dba2d97f3b3693
nickleezx <nickleezx@gmail.com>
2025-09-01 00:13:17 +0800

    ex 9.10 - 9.11 completed

--- part9/patientor_backend/index.ts
+++ part9/patientor_backend/index.ts
@@ -1,10 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-
+import express from "express";
+import cors from "cors";
+import diagnosesRouter from "./routers/diagnoses";
+import patientRouter from "./routers/patients";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/diagnoses", diagnosesRouter);
+app.use("/api/patients", patientRouter);
 
 app.get("/api/ping", (_req, res) => {
     console.log("Someone pinged here");
---
+++ part9/patientor_backend/services/patientService.ts
@@ -0,0 +1,22 @@
+import patients from "../data/patients";
+import { Patient, PatientWithoutSsn } from "../types";
+
+const getPatients = (): Patient[] => {
+    return patients;
+};
+
+const getPatientsWithoutSsn = (): PatientWithoutSsn[]=> {
+    console.log("here")
+    return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
+        id,
+        name,
+        dateOfBirth,
+        gender,
+        occupation
+    }));
+};
+
+export default {
+    getPatients,
+    getPatientsWithoutSsn
+};
